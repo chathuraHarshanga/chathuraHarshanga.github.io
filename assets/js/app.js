0aedadf616b7caae03d6899a88f98dde0b7e0794
chathuraHarshanga <chathura749@gmail.com>
2021-12-14 16:48:13 +0530

    navibar mobile

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navi-links');
+    const navLinks = document.querySelectorAll('.navi-links li');
+
+    burger.addEventListener('click', () => {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //animation links
+        navLinks.forEach((link,index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        //burguer animation
+        burger.classList.toggle('toggle');
+    });
+
+    
+}
+
+navSlide();
